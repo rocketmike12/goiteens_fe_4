87f1f5d8bd7e9daf604f0b473ca1854403812925
rocketmike12 <mg.korop@gmail.com>
2025-05-04 12:32:49 +0300

    attempt lesson17_products fix

--- lesson17_products/script.js
+++ lesson17_products/script.js
@@ -1,7 +1,8 @@
 const productList = document.querySelector(".product-list");
+const filterInput = document.querySelector(".filter");
 
 const loadBtn = document.createElement("button");
-loadBtn.classList.add('load-btn');
+loadBtn.classList.add("load-btn");
 loadBtn.textContent = "load more";
 document.body.appendChild(loadBtn);
 
@@ -9,40 +10,53 @@ const renderQuantity = 10;
 let productIndex = 0;
 let products = [];
 
-const renderProducts = function () {
-    products.slice(productIndex, productIndex + renderQuantity).forEach((el) => {
-        let product = document.createElement("li");
-        product.innerHTML = `
-            <h2>${el.name}</h2>
-            <img src="${el.img}">
-            <p class="description">${el.description}</p>
-            <p class="price">${el.price}$</p>
-        `;
-        productList.appendChild(product);
-        productIndex++;
-    });
-    if (productIndex + renderQuantity > products.length) {
-        document.body.removeChild(loadBtn);
-        Toastify({
-            text: "all products loaded",
-            duration: 3000,
-            close: true,
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-                background: "blueviolet",
-                padding: "20px",
-                fontSize: "20"
-            },
-            onClick: function () {}, // Callback after click
-        }).showToast();
-    }
+const renderProducts = function (isFiltered, data) {
+	productList.innerHTML = "";
+	data.slice(0, productIndex + renderQuantity).forEach((el) => {
+		let product = document.createElement("li");
+		product.innerHTML = `
+            	<h2>${el.name}</h2>
+            	<img src="${el.img}">
+            	<p class="description">${el.description}</p>
+            	<p class="price">${el.price}$</p>
+				<button class="delete-btn">delete</button>
+        	`;
+		productList.appendChild(product);
+		if (!isFiltered) {
+			productIndex++;
+		}
+	});
+	if (productIndex + renderQuantity > products.length) {
+		document.body.removeChild(loadBtn);
+		Toastify({
+			text: "all products loaded",
+			duration: 3000,
+			close: true,
+			stopOnFocus: true,
+			style: {
+				background: "blueviolet",
+				padding: "20px",
+				fontSize: "20",
+			},
+		}).showToast();
+	}
 };
 
 fetch("https://67fa491e8ee14a5426274531.mockapi.io/data/products")
-    .then((res) => res.json())
-    .then((res) => {
-        products = res;
-        renderProducts();
-    });
+	.then((res) => res.json())
+	.then((res) => {
+		products = res;
+		renderProducts(false, products);
+	});
 
-loadBtn.addEventListener("click", renderProducts);
+loadBtn.addEventListener("click", () => {
+	renderProducts(false, products);
+});
+
+filterInput.addEventListener("input", () => {
+	productIndex = 0;
+	renderProducts(
+		true,
+		products.filter((el) => el.name.toLowerCase().includes(filterInput.value.toLowerCase())),
+	);
+});
